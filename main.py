a5d3eb9efa5f364f5632b37411815ec5772f4acd
loganmbleyl05@gmail.com <loganmbleyl05@gmail.com>
2026-03-08 20:27:41 -0400

    Main.py done working on UI

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from poke-guesser!")
+import pygame
+from game.game_manager import GameManager
+from game.ui import UI
 
+
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Who's That Pokémon?")
+    clock = pygame.time.Clock()
+    game_manager = GameManager()
+    ui = UI(screen)
+    
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            elif event.type == pygame.KEYDOWN:
+                game_manager.handle_event(event)
+        game_manager.update()
+        state = game_manager.get_state()
+        ui.draw(state)
+        clock.tick(60)
+       
+       
 
 if __name__ == "__main__":
     main()
